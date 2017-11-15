2246e643a1860bfc23659903f5b854d9030dd787
Kevin Hagens <kevinh@airespot.com>
2017-11-15 13:56:09 -0600

    holds data

---
+++ model/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	ping		: 'OK',
+	status		: 'YES',
+	position	: 'Balihoo Enginner',
+	years		: 4,
+	phone		: '314-258-9017',
+	name		: 'Kevin Hagens Jr',
+	source		: 'http://github.com/kevinhagensjr/question',
+	referrer	: 'indeed.com',
+	degree		: 'Computer Science, Concordia University Chicago',
+	solve		: 'ABCD A=<>> B>=>> C<<=< D<<>=',
+	resume		: 'http://github.com/kevinhagensjr/question/docs/resume.docx',
+	email		: 'khagens92@gmail.com'
+};
