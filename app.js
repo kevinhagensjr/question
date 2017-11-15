c7f1c37476fe00cb44e516dce82ee6a0be4f33b4
Kevin Hagens <kevinh@airespot.com>
2017-11-15 12:47:25 -0600

    config path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('./conf/config');
+const config = require('./config');
 
 class QuestionAPI{
 	
