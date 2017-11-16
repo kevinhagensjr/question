c7d26b852d7a911bde3327634d8dac5f3e4dbcaf
Kevin Hagens <kevinh@airespot.com>
2017-11-16 12:33:23 -0600

    logging for testing

--- controller/index.js
+++ controller/index.js
@@ -70,7 +70,7 @@ class IndexController{
 		
 		//strip instructions from question, turn into array
 		const strippedInstructions = question.replace('Please solve this puzzle:', '');
-		const expressions = strippedInstructions.split('\n').shift().pop();
+		const expressions = strippedInstructions.split('\n');
 		console.log(expressions.length);
 		const letters     = expressions[0].split(''); //get original letter order
 		
